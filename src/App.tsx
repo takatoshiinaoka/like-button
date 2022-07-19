6550accbe07dfc400d0b6274bb992c83b182b6bf
rice-hill <heavenhayao2316@gmail.com>
2022-07-19 16:52:56 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LikeButton />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+function LikeButton(){
+  const [count, setCount] = useState(999)
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  return (
+    <span className='likeButton' onClick={handleClick}>
+      ❤︎ {count}
+    </span>
+  )
+}
+
+export default App
